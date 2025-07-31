49502acc5f870d86aa482ce5a1c119518a379bfa
Joaquin Polonuer <jtpolonuer@gmail.com>
2025-07-31 12:14:06 -0300

    added some utils

---
+++ src/utils.py
@@ -0,0 +1,23 @@
+import torch
+import pandas as pd
+from config import DATA_DIR
+from graph_types.prime import PrimeGraph
+from graph_types.mag import MagGraph
+
+
+def load_graph_and_qas(graph_name: str):
+    qas = pd.read_csv(DATA_DIR / f"02_qa_datasets/{graph_name}.csv")
+    if graph_name == "prime":
+        graph = PrimeGraph.load()
+    elif graph_name == "mag":
+        graph = MagGraph.load()
+    return graph, qas
+
+def load_embeddings(graph_name: str):
+    doc_embeddings = torch.load(
+        DATA_DIR / f"node_embeddings/{graph_name}/text-embedding-ada-002/doc/candidate_emb_dict.pt"
+    )
+    query_embeddings = torch.load(
+        DATA_DIR / f"node_embeddings/{graph_name}/text-embedding-ada-002/query/query_emb_dict.pt"
+    )
+    return doc_embeddings, query_embeddings
