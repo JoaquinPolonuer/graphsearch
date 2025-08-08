44b6b56d5b76febd79d043d6b6682f9a0c18a8a1
Joaquin Polonuer <jtpolonuer@gmail.com>
2025-08-08 17:09:33 -0300

    started refactoring

--- src/experiments/subgraph_explorer.py
+++ src/experiments/subgraph_explorer.py
@@ -1,9 +1,6 @@
-import json
 import os
 import sys
 from pathlib import Path
-
-import torch
 
 sys.path.append(str(Path(__file__).parent.parent.parent))
 
@@ -11,16 +8,16 @@ from graph_types.graph import Node
 from src.llms.simple_calls import extract_entities_from_question, filter_relevant_nodes
 from src.llms.agents.subgraph_explorer import SubgraphExplorerAgent
 from src.utils import iterate_qas, load_embeddings, load_graph_and_qas, setup_results_dir
+from src.experiments.utils import semantic_sort, map_entities_to_nodes, save_log, send_explorers
 
-graph_name = "amazon"
+graph_name = "prime"
 doc_embeddings, query_embeddings = load_embeddings(graph_name)
 graph, qas = load_graph_and_qas(graph_name)
 
-results_dir = setup_results_dir(graph.name, "subgraph_explorer")
+results_dir = setup_results_dir(graph.name, "subgraph_explorer_")
 for question_index, question, answer_indices in list(iterate_qas(qas, limit=1000, shuffle=True))[
     :100
 ]:
-
     if os.path.exists(results_dir / f"{question_index}.json"):
         # with open(results_dir / f"{question_index}.json", "r") as f:
         #     log = json.load(f)
@@ -34,64 +31,29 @@ for question_index, question, answer_indices in list(iterate_qas(qas, limit=1000
 
     entities = extract_entities_from_question(question)
 
-    all_nodes = []
-    all_scores = []
-    for entity in entities:
-        nodes, scores = graph.search_nodes(entity, k=3)
-        all_nodes.extend(nodes)
-        all_scores.extend(scores)
-
+    all_nodes, all_scores = map_entities_to_nodes(graph, entities)
     starting_nodes = filter_relevant_nodes(question, all_nodes, graph)
 
-    message_histories = []
-    agent_answer_nodes: set[Node] = set()
-    for starting_node in starting_nodes:
-        subgraph = graph.get_khop_subgraph(starting_node, k=2)
-
-        # subgraph.nodes_df["similarity"] = subgraph.nodes_df["index"].apply(
-        #     lambda x: torch.matmul(
-        #         query_embeddings[question_index].detach().clone(),
-        #         doc_embeddings[x].detach().clone().T,
-        #     ).item()
-        # )
-
-        # subgraph.nodes_df = subgraph.nodes_df.sort_values(by="similarity", ascending=False)
-
-        agent = SubgraphExplorerAgent(
-            node=starting_node,
-            graph=subgraph,
-            question=question,
-        )
-        for selected_tool, final_answer in agent.answer():
-            if final_answer is not None:
-                agent_answer_nodes = agent_answer_nodes.union(set(final_answer))
-                break
-        message_histories.append(agent.message_history)
-
-    agent_answer_indices = [node.index for node in agent_answer_nodes]
+    message_histories, agent_answer_indices = send_explorers(graph, question, starting_nodes)
 
     if graph.name == "mag":
         agent_answer_indices = graph.filter_indices_by_type(agent_answer_indices, "paper")
     if graph.name == "amazon":
         agent_answer_indices = graph.filter_indices_by_type(agent_answer_indices, "product")
 
-    agent_answer_indices = sorted(
-        agent_answer_indices,
-        key=lambda x: torch.matmul(
-            query_embeddings[question_index].detach().clone(),
-            doc_embeddings[x].detach().clone().T,
-        ).item(),
-        reverse=True,
+    agent_answer_indices = semantic_sort(
+        agent_answer_indices, query_embeddings[question_index], doc_embeddings
     )
 
-    log = {
-        "question": question,
-        "all_nodes": [node.index for node in all_nodes],
-        "message_histories": message_histories,
-        "starting_nodes_indices": [node.index for node in starting_nodes],
-        "agent_answer_indices": agent_answer_indices,
-        "answer_indices": answer_indices,
-    }
-
-    with open(results_dir / f"{question_index}.json", "w") as f:
-        json.dump(log, f, indent=4)
+    save_log(
+        {
+            "question": question,
+            "all_nodes": [node.index for node in all_nodes],
+            "message_histories": message_histories,
+            "starting_nodes_indices": [node.index for node in starting_nodes],
+            "agent_answer_indices": agent_answer_indices,
+            "answer_indices": answer_indices,
+        },
+        results_dir=results_dir,
+        question_index=question_index,
+    )
---
+++ src/experiments/utils.py
@@ -0,0 +1,56 @@
+import json
+import sys
+from pathlib import Path
+
+import torch
+
+sys.path.append(str(Path(__file__).parent.parent.parent))
+
+from graph_types.graph import Node
+from src.llms.agents.subgraph_explorer import SubgraphExplorerAgent
+
+
+def semantic_sort(indices, query_embedding, doc_embeddings):
+    return sorted(
+        indices,
+        key=lambda x: torch.matmul(
+            query_embedding.detach().clone(),
+            doc_embeddings[x].detach().clone().T,
+        ).item(),
+        reverse=True,
+    )
+
+
+def send_explorers(graph, question, starting_nodes):
+    message_histories = []
+    agent_answer_nodes: set[Node] = set()
+    for starting_node in starting_nodes:
+        subgraph = graph.get_khop_subgraph(starting_node, k=2)
+        agent = SubgraphExplorerAgent(
+            node=starting_node,
+            graph=subgraph,
+            question=question,
+        )
+        for selected_tool, final_answer in agent.answer():
+            if final_answer is not None:
+                agent_answer_nodes = agent_answer_nodes.union(set(final_answer))
+                break
+        message_histories.append(agent.message_history)
+
+    agent_answer_indices = [node.index for node in agent_answer_nodes]
+    return message_histories, agent_answer_indices
+
+
+def map_entities_to_nodes(graph, entities):
+    all_nodes = []
+    all_scores = []
+    for entity in entities:
+        nodes, scores = graph.search_nodes(entity, k=1)
+        all_nodes.extend(nodes)
+        all_scores.extend(scores)
+    return all_nodes, all_scores
+
+
+def save_log(log, results_dir, question_index):
+    with open(results_dir / f"{question_index}.json", "w") as f:
+        json.dump(log, f, indent=4)
